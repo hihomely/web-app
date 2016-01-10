a94fbba1e14fca72d3fa83052ceb4e175409bd61
Derek Bekoe <derek@derekbekoe.com>
2016-01-09 16:38:50 -0800

    Template for js config file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 # Create config for nginx
 j2 /templates/nginx.conf.j2 > /etc/nginx/nginx.conf
+# Create javascript web app config
+:${OAUTH_GOOGLE_ID:?"OAUTH_GOOGLE_ID is required and must be non-empty"}
+:${OAUTH_WINDOWS_ID:?"OAUTH_WINDOWS_ID is required and must be non-empty"}
+j2 /templates/js.config.j2 > /usr/share/nginx/html/js/config.js
 exec "$@"
